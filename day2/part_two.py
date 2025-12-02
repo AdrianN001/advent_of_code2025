7a3173dcd6f67a0f2bfe8bc5cb2c2202691f74ba
adrian <gogos.adrian2006@gmail.com>
2025-12-02 14:07:42 +0100

    .

--- day2/part_two.py
+++ day2/part_two.py
@@ -1,4 +1,6 @@
 import math
+
+
 def parse_input(path: str) -> list[str]:
     with open(path, "r") as f:
         return [chunk.strip() for chunk in f.read().split(",") if chunk.strip()]
@@ -6,7 +8,21 @@ def parse_input(path: str) -> list[str]:
 def check_if_invalid(n: int) -> bool:
     s = str(n)
     L = len(s)
-    return L % 2 == 0 and s[:L//2] == s[L//2:]        
+
+    pattern = str()
+    matches = 0
+
+    for i in range(1,L): 
+        if (L % i) != 0:
+            continue
+        pattern = s[: i]
+
+        potential_occur = L//i
+        matches = (pattern * potential_occur) == s
+        
+        if matches and potential_occur >= 2:
+            return True
+    return False
 
 
 
@@ -25,7 +41,6 @@ def main() -> None:
         for _id in range(start,end+1):
             
             if check_if_invalid(_id):
-                print(_id)
                 invalid_ids_sum += _id
 
     print(invalid_ids_sum)
